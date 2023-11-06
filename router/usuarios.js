e2f8c353a0674ec5ecfc93dbad4a8b3970083c1f
Bastian Rozas <bastianrozas000@gmail.com>
2023-11-06 11:34:12 -0300

    Primer backend basico

---
+++ router/usuarios.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const {usuariosGet,
+    usuariosPut,
+    usuariosPatch,
+    usuariosPost,
+    usuariosDelete} = require('../controllers/usuarios');
+const router = Router();
+
+router.get('/', usuariosGet);
+
+router.put('/:id', usuariosPut);
+
+router.patch('/', usuariosPatch);
+
+router.post('/', usuariosPost);
+
+router.delete('/', usuariosDelete);
+
+module.exports = router;
